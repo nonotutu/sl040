e0e884faabc3b6cb7bb82001980cba7cdfbb944c
vagrant <vagrant@rails-dev-box>
2013-06-13 22:02:37 +0000

    application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Sl040
   class Application < Rails::Application
 
     # arno : for Heroku deployement since Devise
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
